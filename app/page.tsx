ad43322f66bd5383b0a609eb50153ed42979497b
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-03-26 18:38:43 +0900

    2403261838

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
-        {["강아지", "고양이", "거북이", "나"].map((person, index) => (
-          <div key={index} className="flex items-center gap-5">
-            <div className="size-10 bg-blue-400 rounded-full" />
-            <span className="text-lg font-medium">{person}</span>
-            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
-              <span className="z-10">{index}</span>
-              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
-            </div>
-          </div>
-        ))}
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+        <div className="group flex flex-col">
+          <input
+            className="bg-gray-100 w-full"
+            placeholder="이메일을 입력하세요"
+          />
+          <span className="group-focus-within:block hidden">
+            올바른 이메일 형식이 아닙니다.
+          </span>
+          <button>입력</button>
+        </div>
       </div>
     </main>
   );
