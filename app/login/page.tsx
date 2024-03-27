098005c285840eb55de10f0cf7627989812b2390
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-03-28 02:32:10 +0900

    2403280232 server actions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,15 +3,26 @@ import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function Login() {
+  const handleForm = async (formData: FormData) => {
+    "use server";
+    console.log(formData.get("email"), formData.get("password"));
+  };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">이메일과 비밀번호로 로그인 해 주세요.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="이메일" required errors={[]} />
+      <form action={handleForm} className="flex flex-col gap-3">
         <FormInput
+          name="email"
+          type="email"
+          placeholder="이메일"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="password"
           type="password"
           placeholder="비밀번호"
           required
