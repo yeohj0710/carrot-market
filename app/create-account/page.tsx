e705f5e087d459e50599471b4593b4abff1792c9
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-03-27 23:08:32 +0900

    2403272308

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,3 +1,5 @@
+import FormButton from "@/components/form-btn";
+import FormInput from "@/components/form-input";
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
@@ -11,16 +13,21 @@ export default function CreateAccount() {
         </h2>
       </div>
       <form className="flex flex-col gap-3">
-        <div className="flex flex-col gap-2">
-          <input
-            className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
-            type="text"
-            placeholder="닉네임"
-            required
-          />
-          <span className="text-red-500 font-medium">Input error</span>
-        </div>
-        <button className="primary-btn h-10">계정 만들기</button>
+        <FormInput type="text" placeholder="닉네임" required errors={[]} />
+        <FormInput type="email" placeholder="이메일" required errors={[]} />
+        <FormInput
+          type="password"
+          placeholder="비밀번호"
+          required
+          errors={[]}
+        />
+        <FormInput
+          type="password"
+          placeholder="비밀번호 확인"
+          required
+          errors={[]}
+        />
+        <FormButton loading={false} text="계정 만들기" />
       </form>
       <div className="w-full h-px bg-neutral-500" />
       <div>
