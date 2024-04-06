68acd1a416d7f96a062ab160a8358b746031618d
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-04-06 17:54:23 +0900

    2404061754

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,13 +2,47 @@
 
 import { z } from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "전화번호 형식이 올바르지 않습니다."
+  );
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogin(prevState: any, formData: FormData) {
-  console.log(typeof formData.get("token"));
-  const parsedToken = tokenSchema.parse(formData.get("token"));
-  console.log(typeof parsedToken);
+interface ActionState {
+  token: boolean;
+}
+
+export async function smsLogin(prevState: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      console.log(result.error.flatten());
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect("/");
+    }
+  }
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,8 +5,13 @@ import Input from "@/components/input";
 import { useFormState } from "react-dom";
 import { smsLogin } from "./actions";
 
+const initialState = {
+  token: false,
+  error: undefined,
+};
+
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsLogin, null);
+  const [state, dispatch] = useFormState(smsLogin, initialState);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -14,16 +19,26 @@ export default function SMSLogin() {
         <h2 className="text-xl">전화번호를 인증해 주세요.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="number" placeholder="전화번호" required />
-        <Input
-          name="token"
-          type="number"
-          placeholder="인증번호"
-          required
-          min={100000}
-          max={999999}
-        />
-        <Button text="인증하기" />
+        {state.token ? (
+          <Input
+            name="token"
+            type="number"
+            placeholder="인증번호"
+            required
+            min={100000}
+            max={999999}
+            errors={state.error?.formErrors}
+          />
+        ) : (
+          <Input
+            name="phone"
+            type="number"
+            placeholder="전화번호"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? "인증하기" : "인증번호 받기"} />
       </form>
     </div>
   );
