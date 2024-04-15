adee2407ac7ec618e36e0f34b60adb78ba176ca0
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-04-15 13:37:04 +0900

    2404151337

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
 
 export async function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  if (pathname === "/") {
-    const response = NextResponse.next();
-    response.cookies.set("middleware-cookie", "hello");
-    return response;
-  }
-  if (pathname === "/profile") {
-    return NextResponse.redirect(new URL("/", request.url));
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
   }
 }
 
 export const config = {
-  matcher: ["/((?!api|_next|/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next|/static|_next/image|favicon.ico).*)"],
 };
