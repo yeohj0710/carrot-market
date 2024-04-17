0cbdebc36e66043580aa2aa0e5bd23c4d93a65ee
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-04-18 01:08:36 +0900

    2404180108

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,11 +1,65 @@
 "use server";
 
-export async function uploadProduct(formData: FormData) {
+import { z } from "zod";
+import fs from "fs/promises";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const productSchema = z.object({
+  photo: z.string({
+    required_error: "사진이 업로드되지 않았습니다.",
+  }),
+  title: z
+    .string({
+      required_error: "상품명이 입력되지 않았습니다.",
+    })
+    .max(50),
+  description: z
+    .string({
+      required_error: "설명이 입력되지 않았습니다.",
+    })
+    .max(500),
+  price: z.coerce.number({
+    required_error: "가격이 입력되지 않았습니다.",
+  }),
+});
+
+export async function uploadProduct(_: any, formData: FormData) {
   const data = {
     photo: formData.get("photo"),
     title: formData.get("title"),
     price: formData.get("price"),
     description: formData.get("description"),
   };
-  console.log(data);
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer();
+    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
+    data.photo = `/${data.photo.name}`;
+  }
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const session = await getSession();
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          description: result.data.description,
+          price: result.data.price,
+          photo: result.data.photo,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      redirect(`/products/${product.id}`);
+    }
+  }
 }
--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -5,6 +5,7 @@ import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { ChangeEvent, useState } from "react";
 import { uploadProduct } from "./actions";
+import { useFormState } from "react-dom";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
@@ -16,12 +17,20 @@ export default function AddProduct() {
       return;
     }
     const file = files[0];
+    if (!file) {
+      return;
+    }
+    if (file.size > 10 * 1024 * 1024) {
+      alert("10MB를 초과하는 이미지는 업로드할 수 없습니다.");
+      return;
+    }
     const url = URL.createObjectURL(file);
     setPreview(url);
   };
+  const [state, action] = useFormState(uploadProduct, null);
   return (
     <div>
-      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
+      <form action={action} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
           className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover"
@@ -33,7 +42,7 @@ export default function AddProduct() {
             <>
               <PhotoIcon className="w-20" />
               <div className="text-neutral-400 text-sm">
-                사진을 업로드해 주세요.
+                {state?.fieldErrors.photo ?? "사진을 업로드해 주세요."}
               </div>
             </>
           ) : null}
@@ -46,13 +55,26 @@ export default function AddProduct() {
           accept="image/*"
           className="hidden"
         />
-        <Input name="title" type="text" required placeholder="상품명" />
-        <Input name="price" type="number" required placeholder="가격" />
+        <Input
+          name="title"
+          type="text"
+          required
+          placeholder="상품명"
+          errors={state?.fieldErrors.title}
+        />
+        <Input
+          name="price"
+          type="number"
+          required
+          placeholder="가격"
+          errors={state?.fieldErrors.price}
+        />
         <Input
           name="description"
           type="text"
           required
           placeholder="상품에 대한 설명"
+          errors={state?.fieldErrors.description}
         />
         <Button text="업로드" />
       </form>
