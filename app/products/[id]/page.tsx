ea816cd54969904e18faaea0b756fe523d95e9cb
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-04-17 12:21:46 +0900

    2404171221

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -49,7 +49,12 @@ export default async function ProductDetail({
   return (
     <div>
       <div className="relative aspect-square">
-        <Image fill src={product.photo} alt={product.title} />
+        <Image
+          fill
+          className="object-cover"
+          src={product.photo}
+          alt={product.title}
+        />
       </div>
       <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
         <div className="size-10 overflow-hidden rounded-full">
---
+++ components/product-list.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { InitialProducts } from "@/app/(tabs)/products/page";
+import ListProduct from "./list-product";
+import { useState } from "react";
+import { getMoreProducts } from "@/app/(tabs)/products/actions";
+
+interface ProductListProps {
+  initialProducts: InitialProducts;
+}
+
+export default function ProductList({ initialProducts }: ProductListProps) {
+  const [products, setProducts] = useState(initialProducts);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(false);
+  const onLoadMoreClick = async () => {
+    setIsLoading(true);
+    const newProducts = await getMoreProducts(page + 1);
+    if (newProducts.length !== 0) {
+      setPage((prev) => prev + 1);
+      setProducts((prev) => [...prev, ...newProducts]);
+    } else {
+      setIsLastPage(true);
+    }
+    setIsLoading(false);
+  };
+  return (
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
+      {isLastPage ? (
+        "마지막 상품입니다."
+      ) : (
+        <button
+          onClick={onLoadMoreClick}
+          disabled={isLoading}
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+        >
+          {isLoading ? "상품 로드 중..." : "상품 더 보기"}
+        </button>
+      )}
+    </div>
+  );
+}
