5fe34085d108883d380f1180eee51028c26daa95
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-04-29 21:12:09 +0900

    2404292112

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -2,14 +2,13 @@ import ProductList from "@/components/product-list";
 import db from "@/lib/db";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { Prisma } from "@prisma/client";
-import { unstable_cache as nextCache } from "next/cache";
+import { unstable_cache as nextCache, revalidatePath } from "next/cache";
 import Link from "next/link";
 
-const getCachedProducts = nextCache(getInitialProducts, ["home-products"], {
-  revalidate: 10,
-});
+const getCachedProducts = nextCache(getInitialProducts, ["home-products"]);
 
 async function getInitialProducts() {
+  console.log("hit");
   const products = await db.product.findMany({
     select: {
       title: true,
@@ -36,9 +35,16 @@ export const metadata = {
 
 export default async function Products() {
   const initialProducts = await getCachedProducts();
+  const revalidate = async () => {
+    "use server";
+    revalidatePath("/home");
+  };
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      <form action={revalidate}>
+        <button>정보 새로고침</button>
+      </form>
       <Link
         href="/products/add"
         className="bg-orange-500 flex items-center justify-center rounded-full size-14 fixed bottom-28 right-8 text-white transition-colors hover:bg-orange-400"
