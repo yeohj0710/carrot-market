fe59d3d5d803c67a58bff4d1cf8c773c3b5d8f87
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-05-03 12:32:34 +0900

    2405031232

--- components/chat-messages-list.tsx
+++ components/chat-messages-list.tsx
@@ -2,19 +2,58 @@
 
 import { InitialChatMessages } from "@/app/chats/[id]/page";
 import { formatToTimeAgo } from "@/lib/utils";
+import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
+import { createClient } from "@supabase/supabase-js";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const SUPABASE_PUBLIC_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiYmhmcmlmZmtwY2hjbWJpeGx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3MDQ4NzMsImV4cCI6MjAzMDI4MDg3M30.8sEV4hwiT8mOZOgGFkVU1vPmlbvWV4sVxaBu5dB9MkI";
+const SUPABASE_URL = "https://hbbhfriffkpchcmbixlx.supabase.co";
 
 interface ChatMessageListProps {
   initialMessages: InitialChatMessages;
   userId: number;
+  chatRoomId: string;
 }
 
 export default function ChatMessegesList({
   initialMessages,
   userId,
+  chatRoomId,
 }: ChatMessageListProps) {
   const [messages, setMessages] = useState(initialMessages);
+  const [message, setMessage] = useState("");
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+    setMessage(value);
+  };
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setMessages((prevMsgs) => [
+      ...prevMsgs,
+      {
+        id: Date.now(),
+        payload: message,
+        created_at: new Date(),
+        userId,
+        user: {
+          nickname: "string",
+          avatar: "string",
+        },
+      },
+    ]);
+    setMessage("");
+  };
+  useEffect(() => {
+    const client = createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY);
+    const channel = client.channel(`room-${chatRoomId}`);
+    channel.on("broadcast", { event: "message" }, (payload) => {
+      console.log(payload);
+    });
+  }, []);
   return (
     <div className="p-5 flex flex-col gap-5 min-h-screen justify-end">
       {messages.map((message) => (
@@ -51,6 +90,20 @@ export default function ChatMessegesList({
           </div>
         </div>
       ))}
+      <form className="flex relative" onSubmit={onSubmit}>
+        <input
+          required
+          onChange={onChange}
+          value={message}
+          className="bg-transparent rounded-full w-full h-10 focus:outline-none px-5 ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-neutral-50 border-none placeholder:text-neutral-400"
+          type="text"
+          name="message"
+          placeholder="Write a message..."
+        />
+        <button className="absolute right-0">
+          <ArrowUpCircleIcon className="size-10 text-orange-500 transition-colors hover:text-orange-300" />
+        </button>
+      </form>
     </div>
   );
 }
